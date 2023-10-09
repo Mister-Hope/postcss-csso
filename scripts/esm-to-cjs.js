719fcc5e63a3625b24c64d6e90c889c93e23bff9
Mr.Hope <mister-hope@outlook.com>
2023-10-09 17:14:53 +0800

    chore: use pnpm

--- scripts/esm-to-cjs.js
+++ scripts/esm-to-cjs.js
@@ -1,53 +1,54 @@
-import fs from 'fs';
-import { rollup } from 'rollup';
+import fs from "fs";
+import { rollup } from "rollup";
 
 const external = [
-    'assert',
-    'module',
-    'postcss-csso',
-    'csso',
-    'csso/syntax',
-    'css-tree',
-    'postcss',
-    'postcss-nested'
+  "assert",
+  "module",
+  "postcss-csso",
+  "csso",
+  "csso/syntax",
+  "css-tree",
+  "postcss",
+  "postcss-nested",
 ];
 
 function readDir(dir) {
-    return fs.readdirSync(dir)
-        .filter(fn => fn.endsWith('.js'))
-        .map(fn => `${dir}/${fn}`);
+  return fs
+    .readdirSync(dir)
+    .filter((fn) => fn.endsWith(".js"))
+    .map((fn) => `${dir}/${fn}`);
 }
 
 async function build(outputDir, ...entryPoints) {
-    const startTime = Date.now();
+  const startTime = Date.now();
 
-    console.log();
-    console.log(`Convert ESM to CommonJS (output: ${outputDir})`);
+  console.log();
+  console.log(`Convert ESM to CommonJS (output: ${outputDir})`);
 
-    const res = await rollup({
-        external,
-        input: entryPoints
-    });
-    await res.write({
-        dir: outputDir,
-        entryFileNames: '[name].cjs',
-        format: 'cjs',
-        exports: 'auto',
-        preserveModules: true,
-        interop: false,
-        esModule: false,
-        generatedCode: {
-            constBindings: true
-        }
-    });
-    await res.close();
+  const res = await rollup({
+    external,
+    input: entryPoints,
+  });
+  await res.write({
+    dir: outputDir,
+    entryFileNames: "[name].cjs",
+    format: "cjs",
+    exports: "auto",
+    preserveModules: true,
+    interop: "auto",
+    esModule: false,
+    generatedCode: {
+      constBindings: true,
+    },
+  });
+  await res.close();
 
-    console.log(`Done in ${Date.now() - startTime}ms`);
+  console.log(`Done in ${Date.now() - startTime}ms`);
 }
 
 async function buildAll() {
-    await build('./cjs', 'lib/index.js');
-    await build('./cjs-test', ...readDir('test'));
+  await build("./cjs", "lib/index.js");
+  await build("./cjs-test", ...readDir("test"));
 }
 
 buildAll();
